f5bdf94ba6aeff6851dd01187813ef49dbacaebb
Abhishek <singhchauhanabhishek816@gmail.com>
2024-02-17 15:03:50 +0530

    First Commit

---
+++ src/main/java/com/example/chatroom/ChatRoom/App/ChatRoomAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.chatroom.ChatRoom.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/chatroom/ChatRoom/App/ChatRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatroom.ChatRoom.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
