a244182907c15eec9bb3e473b23af72190822b18
Abhishek <singhchauhanabhishek816@gmail.com>
2024-02-17 18:19:55 +0530

    fixed some bugs

--- src/main/java/com/example/chatroom/ChatRoom/App/configurations/Config.java
+++ src/main/java/com/example/chatroom/ChatRoom/App/configurations/Config.java
@@ -12,7 +12,7 @@ public class Config implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/server1").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/server1").withSockJS();
     }
 
     @Override
